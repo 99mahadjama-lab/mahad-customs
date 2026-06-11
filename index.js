bf89520951d98d97964489744fd6ba6ad23d9a8a
Mahad Jama <99mahadjama@gmail.com>
2026-06-11 18:20:20 +0100

    update4

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const core = require('./@actions/core')
+const core = require('./node_modules/@actions/core')
 
 try {
     const nameToGreet = core.getInput('who-to-greet');
@@ -7,4 +7,3 @@ try {
 } catch (error) {
     core.setFailed(error.message);
 }
-
